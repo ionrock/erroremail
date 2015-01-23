e4f5f827e3c06ba2a1ca6bcddb7fc08725c207f6
Eric Larson <eric@ionrock.org>
2015-01-22 20:20:46 -0600

    Bump version: 0.1.5 → 0.1.6

--- erroremail/__init__.py
+++ erroremail/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Eric Larson'
 __email__ = 'eric@ionrock.org'
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 
 import cgitb
 import smtplib
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = []
 
 setup(
     name='erroremail',
-    version='0.1.5',
+    version='0.1.6',
     description='Send an error email when an you get an exception.',
     long_description=readme,
     author='Eric Larson',
